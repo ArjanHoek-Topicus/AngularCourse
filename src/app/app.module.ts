238ce863d4d7a3333bf6ee89a42ea182ec6e045b
Arjan Hoek <132347615+ArjanHoek-Topicus@users.noreply.github.com>
2023-07-12 16:07:25 +0200

    remove content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    FormsModule, // Import the FormsModule to enable form features, like [(ngModel)] used in the app component template
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
