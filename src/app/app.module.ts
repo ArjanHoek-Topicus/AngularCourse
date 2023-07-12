43dc52d223d4701db1b19c0e3fc33b53ebaabc4a
Arjan Hoek <132347615+ArjanHoek-Topicus@users.noreply.github.com>
2023-07-12 16:02:48 +0200

    remove unnecessary content and add simple input functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // Import the FormsModule to enable form features, like [(ngModel)] used in the app component template
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
